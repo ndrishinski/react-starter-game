79160b4cb8b820f34a67075370851fdb77f473e1
ndrishinski <ndrishinski@gmail.com>
2024-05-06 19:01:28 -0400

    Succesfully adding Xs to the board

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-export default function Square() {
+import { useState } from 'react';
+
+function Square({value, onSquareClicked}) {
+  return (
+    <button className="square" onClick={onSquareClicked}>
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);    
+  }
   return (
     <>
-    <div className="board-row">
-      <button className="square">1</button>
-      <button className="square">2</button>
-      <button className="square">3</button>
-    </div>
-    <div className="board-row">
-      <button className="square">4</button>
-      <button className="square">5</button>
-      <button className="square">6</button>
-    </div>
-    <div className="board-row">
-      <button className="square">7</button>
-      <button className="square">8</button>
-      <button className="square">9</button>
-    </div>
-  </>
-  )
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClicked={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClicked={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClicked={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClicked={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClicked={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClicked={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClicked={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClicked={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClicked={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
